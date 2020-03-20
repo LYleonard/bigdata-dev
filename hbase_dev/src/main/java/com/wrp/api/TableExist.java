796dc2953d0b7d82d5a8dde0ece22ac80d46482a
LYleonard <aygxywrp@163.com>
2020-03-20 17:29:10 +0800

    HBase Java API Table isExist

---
+++ hbase_dev/src/main/java/com/wrp/api/TableExist.java
@@ -0,0 +1,32 @@
+package com.wrp.api;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.hbase.HBaseConfiguration;
+import org.apache.hadoop.hbase.TableName;
+import org.apache.hadoop.hbase.client.Admin;
+import org.apache.hadoop.hbase.client.Connection;
+import org.apache.hadoop.hbase.client.ConnectionFactory;
+
+import java.io.IOException;
+
+/**
+ * @ClassName TableExist
+ * @Author LYleonard
+ * @Date 2020/3/20 17:11
+ * @Description HBase Java API 判断hbase表是否存在
+ * Version 1.0
+ **/
+public class TableExist {
+    public static Configuration configuration;
+    public static String tableName;
+    static {
+        configuration = HBaseConfiguration.create();
+        tableName = "myuser2";
+    }
+
+    public boolean isTableExist() throws IOException {
+        Connection connection = ConnectionFactory.createConnection(configuration);
+        Admin admin = connection.getAdmin();
+        return admin.tableExists(TableName.valueOf(tableName));
+    }
+}
