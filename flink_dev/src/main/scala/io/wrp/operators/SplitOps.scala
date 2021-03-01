38b50d0aea2f305c1516561315b6d12d373a1243
lyleonard <wangruping@ictbda.cn>
2021-03-01 16:08:18 +0800

    refactor: update flink version and remove deprecated DataStream#split, using side outputs

--- flink_dev/src/main/scala/io/wrp/operators/SplitOps.scala
+++ flink_dev/src/main/scala/io/wrp/operators/SplitOps.scala
@@ -1,9 +1,8 @@
 package io.wrp.operators
 
-import java.{lang, util}
-
-import org.apache.flink.streaming.api.collector.selector.OutputSelector
-import org.apache.flink.streaming.api.scala.{SplitStream, StreamExecutionEnvironment}
+import org.apache.flink.streaming.api.functions.ProcessFunction
+import org.apache.flink.streaming.api.scala.{DataStream, OutputTag, StreamExecutionEnvironment}
+import org.apache.flink.util.Collector
 
 /**
   * @author LYleonard
@@ -15,9 +14,11 @@ object SplitOps {
     val env = StreamExecutionEnvironment.getExecutionEnvironment
 
     import org.apache.flink.api.scala._
-    val raw = env.fromElements("hello flink", "spark kafka", "flink kafka")
+    val raw: DataStream[String] = env.fromElements("hello flink", "spark kafka", "flink kafka")
 
-    val splitedDS: SplitStream[String] = raw.split(new OutputSelector[String] {
+
+/*    //1.8.1版本Flink Split算子实现
+      val splitedDS: SplitStream[String] = raw.split(new OutputSelector[String] {
       override def select(out: String): lang.Iterable[String] = {
         val strings = new util.ArrayList[String]()
         if (out.contains("hello")){
@@ -31,6 +32,26 @@ object SplitOps {
 
     val result = splitedDS.select("hello")
     result.print().setParallelism(1)
+*/
+
+    // 1.12.0版本Flink 通过Side out实现
+    val outputTag: OutputTag[String] = OutputTag[String]("hello")
+    val result: DataStream[String] = raw.process(new ProcessFunction[String, String] {
+      override def processElement(value: String,
+                                  ctx: ProcessFunction[String, String]#Context,
+                                  out: Collector[String]): Unit = {
+
+        if (value.contains("hello")){
+          out.collect(value)
+        } else {
+          ctx.output(outputTag,value)
+        }
+      }
+    })
+    val siderOutPut: DataStream[String] = result.getSideOutput(outputTag)
+    siderOutPut.print("side out")
+    result.print("hello")
+
     env.execute()
   }
 }
