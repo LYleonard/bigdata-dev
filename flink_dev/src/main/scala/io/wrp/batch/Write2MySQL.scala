6fbd9cf03e47d6f31954317d88ae885dd2fe8cb6
lyleonard <wangruping@ictbda.cn>
2021-03-02 09:38:38 +0800

    refactor: using mysql8 driver

--- flink_dev/src/main/scala/io/wrp/batch/Write2MySQL.scala
+++ flink_dev/src/main/scala/io/wrp/batch/Write2MySQL.scala
@@ -15,16 +15,18 @@ object Write2MySQL {
     import org.apache.flink.api.scala._
     val source = env.fromElements("1,flink", "2,spark", "3,hive")
     val dataSet = source.mapPartition(part => {
-      Class.forName("com.mysql.jdbc.Driver").newInstance()
-      val conn = java.sql.DriverManager.getConnection("jdbc:mysql://localhost:3306/test",
+      Class.forName("com.mysql.cj.jdbc.Driver").newInstance()
+      val conn = java.sql.DriverManager.getConnection(
+        "jdbc:mysql://localhost:3306/test?useSSL=false&serverTimezone=GMT%2B8&useUnicode=true&characterEncoding=UTF-8",
         "root", "123456")
       part.map(x => {
-        val statement: PreparedStatement = conn.prepareStatement("insert into user (id,name) values(?,?)")
+        val statement: PreparedStatement = conn.prepareStatement("insert into user1 (id,name) values(?,?)")
         statement.setInt(1, x.split(",")(0).toInt)
         statement.setString(2, x.split(",")(1))
         statement.execute()
       })
     }).print()
+
     env.execute()
   }
 }
